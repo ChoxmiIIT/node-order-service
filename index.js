6808c0735f3b147b08638f64339ce880155ee9c9
Choxmi <gwcsathsara@gmail.com>
2025-08-07 20:54:35 -0400

    order endpoints

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
 
 db.connect();
 
-app.post('/orders', (req, res) => {
+app.post('/order/orders', (req, res) => {
   const { cart_items, total_price } = req.body;
   db.query(
     'INSERT INTO orders (cart_items, total_price) VALUES (?, ?)',
@@ -24,17 +24,21 @@ app.post('/orders', (req, res) => {
   );
 });
 
-app.get('/orders', (req, res) => {
+app.get('/order/orders', (req, res) => {
   db.query('SELECT * FROM orders', (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
-    results.forEach(order => {
-      order.cart_items = JSON.parse(order.cart_items);
-    });
     res.json(results);
   });
 });
 
-app.get('/', (req, res) => {
+app.get('/order/order/:id', (req, res) => {
+  db.query(`SELECT * FROM orders WHERE id = ${req.params.id}`, (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+});
+
+app.get('/order/', (req, res) => {
     res.send('Welcome to the Order Service');
 });
 
